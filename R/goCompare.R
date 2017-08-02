931fda6bf4b3b4cc22f53b8da92492025687d60c
tomcopple <tomcopple@gmail.com>
2017-08-02 13:42:03 +0100

    Including goCompare function in package.

---
+++ R/goCompare.R
@@ -0,0 +1,133 @@
+#' Go Compare
+#'
+#' Runs a comparison for a specific country of various production estimates, and returns graphs of exports so far and production.
+#'
+#' @param countryName Case sensitive, needs to be a country in the system.
+#'
+#' @export
+#' @import tidyverse
+#' @importFrom readr read_csv
+#' @importFrom forcats fct_relevel
+#' @importFrom plotly plot_ly layout subplot
+#' @importFrom lubridate year month
+#' @importFrom magrittr extract2
+
+goCompare <- function(countryName) {
+
+    library(tidyverse);library(coffeestats)
+
+    coffeestats::setDataDir()
+
+    if(!exists('prodAll')) { suppressMessages(coffeestats::getProduction()) }
+    if(!exists('mts')) { suppressMessages(coffeestats::loadMTS()) }
+    if(!exists('prodOther')) {
+        prodOther <<- suppressMessages(readxl::read_excel(
+            path = file.path(coffeestats, "production-estimates.xlsx")
+        ) %>%
+            select(source = Source, country = Country, year = Year, production = Production) %>%
+            # Don't worry about Arabica/Robusta split for now, just use total.
+            group_by(country, source, year) %>%
+            summarise(value = sum(production)) %>%
+            ungroup()
+        )
+    }
+
+
+    # Import crop year info
+    cy <- suppressMessages(readr::read_csv(
+        file = file.path(coffeestats, "cropyears.csv")
+    ))
+
+    # Put everything together
+    compData <- suppressMessages(
+        bind_rows(
+        prodAll %>% rename(country = Country) %>% mutate(source = "ICO", year = as.numeric(year)),
+        prodOther
+    ) %>%
+        full_join(
+            x = ., y = rename(cy, country = Country, cropYear = Crop.year)
+        ) %>%
+        filter(year >= max(year) - 5) %>%
+        mutate(source = forcats::fct_relevel(source, "ICO"))
+    )
+
+    # Production graph and table
+    tableProd <- filter(compData, country == countryName) %>%
+        arrange(source, year, value)
+    graphProd <- suppressWarnings(plotly::plot_ly(
+        tableProd %>% group_by(source),
+        type = "scatter", mode = "lines",
+        x = ~year, y = ~value, color = ~source,
+        text = ~paste0(source, ": ", round(value, 1)), hoverinfo = "text"
+    ) %>%
+        plotly::layout(
+            title = ~paste("Production by", country),
+            xaxis = list(title = "", showgrid = FALSE),
+            yaxis = list(title = "", zeroline = FALSE)
+        )
+    )
+
+
+    # Some countries are missing in the exports file, so just use production
+    if(!countryName %in% mts$country) {
+        print(graphProd)
+        print(tableProd %>% mutate(value = round(value, 0)) %>% spread(key = year, value = value))
+        return("No export data found")
+    }
+
+    # Otherwise need to do monthly exports by crop year
+    ## First get month number
+    cyNum <- compData %>%
+        filter(country == countryName) %>%
+        slice(1) %>%
+        magrittr::extract2('cropYear') %>%
+        match(., month.abb)
+    if(length(cyNum) == 0) {
+        return("Error in crop year lookup")
+    }
+
+    ## Create an ordered factor of months starting with the crop year
+    monthFac <- c(month.abb, month.abb)[cyNum:(cyNum + 11)]
+
+    ## Now create a new dataframe just for the specific country
+    compExp <- mts %>%
+        filter(country == countryName) %>%
+        mutate(
+            monthFac = ordered(month.abb[lubridate::month(month)], levels = monthFac)
+        ) %>%
+        arrange(monthFac, lubridate::year(month)) %>%
+        filter(month >= month[1]) %>%
+        arrange(month) %>%
+        mutate(cropYear = as.factor(
+            c(
+                rep(lubridate::year(min(month)):((lubridate::year(min(month)) + nrow(.) %/% 12) -1 ),
+                    each = 12),
+                rep(lubridate::year(min(month)) + nrow(.) %/% 12,
+                    each = nrow(.) %% 12)
+            )
+        ))
+
+    graphExp <- suppressWarnings(plotly::plot_ly(
+        compExp %>% group_by(cropYear),
+        x = ~monthFac, y = ~value, color = ~cropYear, type = "scatter", mode = "lines"
+    ) %>%
+        plotly::layout(
+            title = ~paste("Exports by", countryName),
+            xaxis = list(title = "", showgrid = FALSE),
+            yaxis = list(title = "Exports", zeroline = FALSE)
+        )
+    )
+
+    tableExp <- compExp %>%
+        filter(monthFac <= last(monthFac)) %>%
+        group_by(cropYear) %>%
+        summarise(exports_to_date = sum(value))
+
+    print(tableExp)
+    print(tableProd %>% mutate(value = round(value, 0)) %>% spread(key = year, value = value))
+
+    suppressWarnings(plotly::subplot(
+        graphExp, graphProd, nrows = 2
+    ))
+}
+
